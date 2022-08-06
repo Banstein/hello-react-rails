f301b133b100ba8a7469130e2edd075453a8d2b2
Islam Bahnas <islambahnas1@gmail.com>
2022-08-06 14:10:05 +0200

    Set redux state store install redux

---
+++ app/javascript/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import HelloWorld from './HelloWorld';
+import configureStore from '../configureStore';
+const store = configureStore();
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" render={()=> ("Home!")} />
+            <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
